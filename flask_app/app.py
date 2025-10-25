e3559cdb4996b866385c61a956e8bd8e4004167f
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-10-25 15:44:39 +0000

    refactor: Flask app coordinate based endpoint and extraction of API logic

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-import requests
-import os
+import api
 
 app = Flask(__name__)
 
@@ -8,6 +7,20 @@ app = Flask(__name__)
 def index():
     # renders templates/index.html
     return render_template('index.html')
+
+@app.route('/submitCoord', methods=['POST'])
+def submit_coord():
+    longitude = request.form.get('longitude')
+    latitude = request.form.get('latitude')
+
+    if not longitude and not latitude:
+        return render_template('index.html', error="Please provide both longitude and latitude.")
+
+    weather_data = api.query_weather(latitude, longitude)
+    if weather_data is None:
+        return render_template('index.html', error="Location not found. Please try again.")
+    
+    return render_template('cityData.html', longitude=longitude, latitude=latitude, weather_data=weather_data)
 
 @app.route('/submitCity', methods=['POST'])
 def submit_city():
@@ -18,23 +31,18 @@ def submit_city():
     if not city and not state and not country:
         return render_template('index.html', error="Please provide at least one of city, state, or country.")
 
-    url = f'https://geocode.maps.co/search?'
-    if city:
-        url += f'city={city}&'
-    if state:
-        url += f'state={state}&'
-    if country:
-        url += f'country={country}&'
-
-    url += f'api_key={os.environ.get("GEOCODE_API_KEY")}'
-    response = requests.post(url)
-    data = response.json()
-    if data is None or len(data) == 0:
+    data = api.query_location(city, state, country)
+    if data is None:
         return render_template('index.html', error="Location not found. Please try again.")
     
     longitude = data[0]['lon']
     latitude = data[0]['lat']
-    return render_template('cityData.html', longitude=longitude, latitude=latitude)
+    
+    weather_data = api.query_weather(latitude, longitude)
+    if weather_data is None:
+        return render_template('index.html', error="Weather data not found. Please try again.")
+    
+    return render_template('cityData.html', longitude=longitude, latitude=latitude, weather_data=weather_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
