bda02121fc8701394dc9084665350cc98f031ed0
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-11-08 15:29:53 +0000

    test: Added tests to increase code coverage in the golden hour classes

--- test/test_.py
+++ test/test_.py
@@ -333,3 +333,133 @@ def test_event_score_visibility_low_min():
     }
     event_data = EventData(json_data)
     assert event_data.calc_golden_score() == 66
+
+def test_event_image_thunderstorm():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 200, 'main': 'Thunderstorm', 'description': 'light intensity drizzle', 'icon': '09d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "thunderstorm"
+
+def test_event_image_drizzle():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 300, 'main': 'Drizzle', 'description': 'light intensity drizzle', 'icon': '09d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "drizzle"
+
+def test_event_image_light_rain():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 500, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "light-rain"
+
+def test_event_image_extreme_rain():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 504, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "extreme-rain"
+
+def test_event_image_sleet():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 511, 'main': 'Snow', 'description': 'freezing rain', 'icon': '13d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "sleet"
+
+def test_event_image_rain():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 501, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "rain"
+
+def test_event_image_sleet_611():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 611, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "sleet"
+
+def test_event_image_snow():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 600, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "snow"
+
+def test_event_image_night():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 1800, 'main': 'Clear Night', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "clear-night"
+
+def test_event_image_night_partial_cloud():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 1801, 'main': 'Clear Night', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "partly-cloudy-night"
+
+def test_event_image_night_cloudy():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 1804, 'main': 'Clear Night', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "cloudy"
+
+def test_event_image_fog():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 700, 'main': 'Mist', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "fog"
+
+def test_event_image_unk():
+    json_data = {
+        'dt': 1762948800, 
+        'main': {'temp': 70}, 
+        'weather': [{'id': 54600, 'main': 'Mist', 'description': 'light rain', 'icon': '10d'}], 
+        'visibility': 0, 
+    }
+    event_data = EventData(json_data)
+    assert event_data.get_image_name() == "unknown"
