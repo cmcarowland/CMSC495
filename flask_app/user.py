df8e05dcdc2887bcbc06733683a320ad9a3c62a8
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-11-13 18:28:57 +0000

    refactor: User to have a from_json static method for loading

--- flask_app/user.py
+++ flask_app/user.py
@@ -2,13 +2,20 @@
 
 class User:
     def __init__(self, username, password_hash):
-        self.username = username
+        self.user_name = username
         self.password_hash = password_hash
         self.favorite_locations = []
+        self.last_login = ''
 
     def to_json(self):
         return {
-            'username': self.username,
+            'username': self.user_name,
             'password_hash': self.password_hash,
             'favorite_locations': self.favorite_locations
         }
+    
+    @staticmethod
+    def from_json(data):
+        user = User(data['username'], data['password_hash'])
+        user.favorite_locations = data.get('favorite_locations', [])
+        return user
