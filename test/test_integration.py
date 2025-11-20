1277bd5eff48448abe50ad0ea29f4c334a7bd7a5
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-11-20 01:00:08 +0000

    refactor: Renamed to integration tests and removed unit tests

---
+++ test/test_integration.py
@@ -0,0 +1,206 @@
+import pytest
+from flask_app import api
+from flask_app.golden_hour_data import EventData
+from flask_app.app import create_app
+
+@pytest.fixture
+def app_fixture():
+    appinst = create_app()
+    appinst.config.update({
+        "TESTING": True,
+    })
+
+    yield appinst
+
+@pytest.fixture
+def client_dummy(app_fixture):
+    return app_fixture.test_client()
+    
+def test_app_creation(app_fixture):   
+    assert app_fixture is not None
+
+def test_appclient_creation(client_dummy):
+    assert client_dummy is not None
+
+def test_index_route(client_dummy):
+    response = client_dummy.get('/')
+    assert response.status_code == 200
+    assert b'<title>GoldenHour</title>' in response.data
+
+def test_city_data(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': 'Los Angeles',
+        'state': 'CA',
+        'country': 'US'
+    })
+    assert response.status_code == 200
+    print(response.data)
+    assert b'Los Angeles, California, US' in response.data
+
+def test_coord_data(client_dummy):
+    response = client_dummy.post('/submitCoord', data={
+        'latitude': '38.9',
+        'longitude': '-77.1'
+    })
+    assert response.status_code == 200
+    assert b'Arlington, Virginia, US' in response.data
+
+
+def test_no_location_information(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': '',
+        'state': '',
+        'country': ''
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide a city, state (US), and country.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_no_city_information(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': '',
+        'state': 'CA',
+        'country': 'US'
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide a valid city.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_no_state_information(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': 'Los Angeles',
+        'state': '',
+        'country': 'US'
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide a state for US locations.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_no_country_information_with_city_and_state(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': 'Los Angeles',
+        'state': 'CA',
+        'country': ''
+    })
+    assert response.status_code == 200
+    assert b'<title>Redirecting...</title>' not in response.data
+    assert b'Los Angeles, California, US' in response.data
+
+def test_no_country_outside_us(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': 'Skopje',
+        'state': '',
+        'country': ''
+    })
+    assert response.status_code == 302
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide a country.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_outside_us(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': 'Skopje',
+        'state': '',
+        'country': 'MK'
+    })
+    assert response.status_code == 200
+    assert b'Skopje, Skopje Region, MK' in response.data
+
+def test_no_coordinates_information(client_dummy):
+    response = client_dummy.post('/submitCoord', data={
+        'latitude': '',
+        'longitude': ''
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide both latitude and longitude.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_no_longitude_information(client_dummy):
+    response = client_dummy.post('/submitCoord', data={
+        'latitude': '38.9',
+        'longitude': ''
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide both latitude and longitude.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_no_latitude_information(client_dummy):
+    response = client_dummy.post('/submitCoord', data={
+        'latitude': '',
+        'longitude': '-77'
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide both latitude and longitude.' in msg for category, msg in flashed_messages if category == 'error')
+
+
+def test_invalid_location(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': 'ThisCityDoesNotExist',
+        'state': 'ZZ',
+        'country': 'US'
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Location not found. Please try again.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_null_city_with_nonus_country(client_dummy):
+    response = client_dummy.post('/submitCity', data={
+        'city': '',
+        'state': '',
+        'country': 'GB'
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Please provide a valid city.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_bad_latitude_information(client_dummy):
+    response = client_dummy.post('/submitCoord', data={
+        'latitude': '625',
+        'longitude': '-77'
+    })
+    assert response.status_code == 302
+    assert b'<title>Redirecting...</title>' in response.data
+    with client_dummy.session_transaction() as session:
+        flashed_messages = session['_flashes']
+        assert any('Location not found. Please try again.' in msg for category, msg in flashed_messages if category == 'error')
+
+def test_current_weather():
+    response = api.query_current_weather(latitude='38.9', longitude='-77.1')
+    assert response is not None
+
+def test_current_weather_bad_data():
+    response = api.query_current_weather(latitude='625', longitude='-77.1')
+    assert response is None
+
+def test_reverse_geocoding():
+    response = api.get_city_name(latitude='38.9', longitude='-77.1')
+    assert response['name'] == 'Arlington'
+
+def test_reverse_geocoding_bad_data():
+    response = api.get_city_name(latitude='625', longitude='-77.1')
+    assert response is None
+
+def test_location_query_bad_data():
+    response = api.query_location(city='ThisCityDoesNotExist', state='ZZ', country='US')
+    assert response is None
+
+def test_location_query_bad_query():
+    response = api.query_location(city='ThisCity,NoDataType=NotExist', state='ZZ', country='US')
+    assert response is None
